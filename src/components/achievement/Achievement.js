050e38c125f0fec754588428801fe6ca5a5c6815
madnooby <amansapra.cs@gmail.com>
2017-03-26 20:21:52 +0000

    achievement added

--- src/components/achievement/Achievement.js
+++ src/components/achievement/Achievement.js
@@ -35,11 +35,16 @@ export default class Achievement extends React.Component{
 
 				<div className="w3 p3">
 					<div className="w3-third img-pad">
-						<img src="img/intel.png" className="w3-image w3-card-8 achv" />
+						<img src="img/intel1.png" className="w3-image w3-card-8 achv" />
 					</div>
 				</div>
 		
-
+				<div className="w3-container">
+					<h4 className="achvText">
+						Apart From these i was also appriciated by amazon, Facebook For Finding a bug which was although not rewared because
+						It was being under fixed by those companies.
+					</h4>
+				</div>
 			</div>
 			);
 	}
